c294d4a3232529a44e1e43fedac763d1d2c81046
silvia730 <silvianjeri730@gmail.com>
2025-12-29 18:07:09 +0300

    Configure Next.js to allow images from fakestoreapi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        pathname: '/img/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
